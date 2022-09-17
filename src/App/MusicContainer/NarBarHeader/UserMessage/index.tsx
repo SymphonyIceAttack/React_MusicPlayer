7a1823770fc800e0e8749f2f885be43ca83be76e
SymphonyIceAttack <1994370030@qq.com>
2022-09-17 23:20:04 +0800

    github-Icons

--- src/App/MusicContainer/NarBarHeader/UserMessage/index.tsx
+++ src/App/MusicContainer/NarBarHeader/UserMessage/index.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import { IoMdNotificationsOutline } from "react-icons/io";
-import { AiOutlineCloud } from "react-icons/ai";
+import { AiOutlineGithub } from "react-icons/ai";
 const index = () => {
     return (
         <div className="m-l-20%  h-60%  flex justify-end items-center hover:color-white">
             <span className="color-teal m-x-12 cursor-pointer hover:color-white">
                 <IoMdNotificationsOutline size={30} />
             </span>
-            <span className="color-teal m-x-12 cursor-pointer hover:color-white">
-                <AiOutlineCloud size={30} />
-            </span>
+            <a
+                href="https://github.com/SymphonyIceAttack/React_MusicPlayer"
+                target={"_blank"}
+                className="color-teal m-x-12 cursor-pointer hover:color-white"
+            >
+                <AiOutlineGithub size={30} />
+            </a>
             <span className="text-18 color-teal hover:color-white cursor-pointer m-x-12 font-500 truncate">
                 登陆
             </span>
